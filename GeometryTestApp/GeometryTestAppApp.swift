e41db39824b3ab51164720565203e2711bba5e08
21cm0129-tumi <91646662+21cm0129-tumi@users.noreply.github.com>
2022-05-10 21:31:49 +0900

    Initial Commit

---
+++ GeometryTestApp/GeometryTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryTestAppApp.swift
+//  GeometryTestApp
+//
+//  Created by cmStudent on 2022/05/10.
+//
+
+import SwiftUI
+
+@main
+struct GeometryTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
